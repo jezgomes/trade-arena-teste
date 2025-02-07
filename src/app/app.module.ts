aa840a3eaa10b8138c74538e801711d8c0278ccc
Jezreel Gomes <jezreelgomes120@gmail.com>
2025-02-07 16:09:22 -0300

    add divider component & add finance section to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DividerComponent } from './components/divider/divider.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, HomeComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    DividerComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
